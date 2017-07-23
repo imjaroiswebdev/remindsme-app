2c8a32bcba5b725f86bfd44f3a5ce6723ecc62e2
José Antonio Reyes <imjaroiswebdev@gmail.com>
2017-07-23 10:38:11 -0400

    Reminder add/del functionality working. Now is turn to implement persistent state for the entered reminders to stay and be usefull, that will be done with cookies.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,24 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addReminder, deleteReminder } from '../actions'
+import moment from 'moment'
 
 class App extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			text: ''
+			text: '',
+			dueDate: ''
 		}
 	}
 
 	addReminder() {
-		// console.log('this', this)
-		this.props.addReminder(this.state.text)
+		console.log('this.state.dueDate', this.state.dueDate)
+		this.props.addReminder(this.state.text, this.state.dueDate)
 	}
 
 	deleteReminder(id) {
-		console.log('deleting in application', id)
-		console.log('this.props', this.props)
+		this.props.deleteReminder(id)
 	}
 
 	renderReminders() {
@@ -28,9 +29,16 @@ class App extends Component {
 					reminders.map(reminder => {
 						return (
 							<li key={reminder.id} className="list-group-item" >
-								<div className="list-item">{reminder.text}</div>
-								<div className="list-item delete-button"
-								onClick={() => this.deleteReminder(reminder.id)}
+								<div className="list-item">
+									<div>{reminder.text}</div>
+									<div><em>
+										{/* Moment method to get relative date in days from today */}
+										{moment(new Date(reminder.dueDate)).fromNow()}
+									</em></div>
+								</div>
+								<div
+									className="list-item delete-button"
+									onClick={() => this.deleteReminder(reminder.id)}
 								>
 									&#x2715;
 								</div>
@@ -54,6 +62,11 @@ class App extends Component {
 							placeholder="I have to..."
 							onChange={event => this.setState({text: event.target.value})}
 						/>
+						<input
+							className="form-control"
+							type="datetime-local"
+							onChange={event => this.setState({dueDate: event.target.value})}
+						 />
 						<button
 							type="button"
 							className="btn btn-success"
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,20 @@
-import { ADD_REMINDER } from '../constants';
+import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
 
 // Helper reminder function
 const reminder = (action) => {
+	let { text, dueDate } = action
 	return {
-		text: action.text,
-		id: Math.random()
+		id: Math.random(),
+		text,
+		dueDate
 	}
+}
+
+// Delete reminders by its id
+const removeById = (state = [], id) => {
+	const reminders = state.filter(reminder => reminder.id !== id)
+	console.log('new reduced reminders', reminders)
+	return reminders
 }
 
 // Reminder reducer
@@ -16,6 +25,9 @@ const reminders = (state = [], action) => {
 			reminders = [...state, reminder(action)]
 			console.log('reminders as state', reminders)
 			return reminders;
+		case DELETE_REMINDER:
+			reminders = removeById(state, action.id)
+			return reminders
 		default:
 			return state
 	}
